8944485140550db74a40e0d4eb86e0b8c26a892c
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-05 16:25:44 -0800

    Properly import the disable input function

--- packages/emoji-mart/src/components/Picker/index.ts
+++ packages/emoji-mart/src/components/Picker/index.ts
@@ -2,4 +2,3 @@
 export { default as Picker } from './Picker'
 export { default as PickerElement } from './PickerElement'
 export { default as PickerStyles } from 'bundle-text:./PickerStyles.scss'
-export * from "./external-utils";
