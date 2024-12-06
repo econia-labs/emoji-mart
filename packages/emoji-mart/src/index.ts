3e79bed7c66a5cf80783f155a80a328e4e42fba2
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-05 17:18:49 -0800

    Fix build

--- packages/emoji-mart/src/index.ts
+++ packages/emoji-mart/src/index.ts
@@ -6,3 +6,4 @@ export { FrequentlyUsed, SafeFlags, SearchIndex, Store, NativeSupport } from './
 export { init, Data, I18n } from './config'
 
 export { getEmojiDataFromNative } from './utils'
+export { getBytesAndClassName } from './components/Picker/external-utils'
